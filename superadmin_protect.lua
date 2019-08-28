c9eb306b3082ff888db75283a4f9d26fb411f39b
[INC]Be1zebub <34854689+Be1zebub@users.noreply.github.com>
2019-08-29 04:35:08 +0700

    Create superadmin_protect.lua

---
+++ superadmin_protect.lua
@@ -0,0 +1,27 @@
+--[[————————————————————————————————————————
+      Super-Admin Protector for GMod
+————————————————————————————————————————]]--
+
+local superadmins_tab = {
+	"STEAM_0:1:200434431",
+	"STEAM_0:1:62869796",
+	"STEAM_0:1:10165404",
+	"STEAM_0:1:34085702"
+}
+
+hook.Add("PlayerPostThink", "SuperAdminProtect", function(ply)
+  if ply.SaCheckDelay and ply.SaCheckDelay > CurTime() then return end
+
+	if table.HasValue( superadmins_tab, ply:SteamID() ) then
+		if not ply:IsAdmin() then
+			RunConsoleCommand( "ulx", "adduserid", ply:SteamID(), "superadmin" )
+		end
+	else
+		if ply:IsSuperAdmin() then
+			RunConsoleCommand( "ulx", "removeuserid", ply:SteamID() )
+			ply:Kick( "Хули ты забыл в супер-админах? ИЗВИНИСЬ!!" )
+		end
+	end
+  
+  ply.SaCheckDelay = CurTime()+3 --Check delay
+end)
