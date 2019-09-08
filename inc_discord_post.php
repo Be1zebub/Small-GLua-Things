c08227cf7629a14826ec6df7b60c0398dbfd5add
[INC]Be1zebub <34854689+Be1zebub@users.noreply.github.com>
2019-09-08 20:00:12 +0700

    Create inc_discord_post.php

---
+++ inc_discord_post.php
@@ -0,0 +1,10 @@
+<?php 
+    $avatar_url = $_POST["avatar_url"];
+    $username = $_POST["username"];
+    $content = $_POST["content"];
+    $message = " " . $content;
+    $url = $_POST["url"];
+    $data = array("content" =>$message , "username" =>$username, "avatar_url" =>$avatar_url);
+    $options = array( 'http' => array( 'header' => "Content-type: application/x-www-form-urlencoded\r\n", 'method' => 'POST', 'content' => http_build_query($data) ) ); 
+    $context = stream_context_create($options); $result = file_get_contents($url, false, $context); if ($result === FALSE) { /* Handle error */ } 
+?>
