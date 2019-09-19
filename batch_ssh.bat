1b0db0aadc180c948d6c0f8a553839b7d607db84
[INC]Be1zebub <34854689+Be1zebub@users.noreply.github.com>
2019-09-19 17:34:36 +0700

    Create batch_ssh.bat

---
+++ batch_ssh.bat
@@ -0,0 +1,25 @@
+Batch SSH Fast&Simple:
+  @echo off
+  :start
+  cls
+  echo Welcome to Incredible SSH!
+  echo Click on any-key for start SSH connection...
+  pause >nul
+  (echo.|set /P="YourSshPasswordHere")| clip  
+  echo SSH Connection with IncredibleVDS has been started!
+  echo Click RMB + Enter for login in!
+  cmd.exe /k ssh User@IP_Adress -p SshPort
+
+
+
+Exapmle:
+  @echo off
+  :start
+  cls
+  echo Welcome to Incredible SSH!
+  echo Click on any-key for start SSH connection...
+  pause >nul
+  (echo.|set /P="123456789qwerty")| clip  
+  echo SSH Connection with IncredibleVDS has been started!
+  echo Click RMB + Enter for login in!
+  cmd.exe /k ssh root@83.12.164.106 -p 22
