a0b45e8232a9ef6b383f23f6895e84de0d08301c
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2019-11-21 20:07:12 +0700

    Update inc_discord_post_lib.lua

--- inc_discord_post_lib.lua
+++ inc_discord_post_lib.lua
@@ -3,10 +3,10 @@
           Credits: [INC]Be1zebub
 
 	Visit my GModDayz Server:
-  steam://connect/p.incredible-gmod.ru
+        http://incredible-gmod.ru
 —————————————————————————————————————]]--
 
-local phpurl = "https://incredible-gmod.ru/inc_discord_post.php" --Link to your .php
+local phpurl = "Your self hosted .php api interface, .php script from - https://github.com/Be1zebub/Small-GLua-Things/blob/master/inc_discord_post.php" --Link to your .php
 local RoflanChelik = "https://i.imgur.com/T3EE95z.png" --Default Avatar (if SteamAPI grants ERROR)
 
 function doWebhookPost(txt, nick, webhook, avatar)
