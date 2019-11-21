dbe95e91771a5076d79b8b69cd00aa8217b43a3d
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2019-11-21 19:32:50 +0700

    Update inc_superadmin_protect.lua

--- inc_superadmin_protect.lua
+++ inc_superadmin_protect.lua
@@ -7,25 +7,40 @@
 ————————————————————————————————————————]]--
 
 local superadmins_tab = {
-	"STEAM_0:1:200434431",
-	"STEAM_0:1:62869796",
-	"STEAM_0:1:10165404",
-	"STEAM_0:1:34085702"
+	["STEAM_0:1:200434431"] = true,
+	["STEAM_0:1:62869796"] = true,
+	["STEAM_0:1:10165404"] = true,
+	["STEAM_0:1:34085702"] = true
 }
+local kick_reason = "Хули ты забыл в супер-админах? ИЗВИНИСЬ!!"
 
 hook.Add("PlayerPostThink", "SuperAdminProtect", function(ply)
-  if (ply.SaCheckDelay or 0) > CurTime() then return end
+	if (ply.SaCheckDelay or 0) > CurTime() then return end
+	ply.SaCheckDelay = CurTime()+2 --Check delay
 
-	if table.HasValue( superadmins_tab, ply:SteamID() ) then
-		if not ply:IsAdmin() then
-			RunConsoleCommand( "ulx", "adduserid", ply:SteamID(), "superadmin" )
+	if superadmins_tab[ply:SteamID()] then
+		if not ply:IsSuperAdmin() then
+			if ULib then
+				RunConsoleCommand( "ulx", "adduserid", ply:SteamID(), "superadmin" )
+			elseif FAdmin then
+				RunConsoleCommand("fadmin", "setaccess", ply:SteamID(), "superadmin")
+			elseif serverguard then
+				serverguard.player:SetRank(ply, "founder")
+			end
 		end
 	else
 		if ply:IsSuperAdmin() then
-			RunConsoleCommand( "ulx", "removeuserid", ply:SteamID() )
-			ply:Kick( "Хули ты забыл в супер-админах? ИЗВИНИСЬ!!" )
+			if ULib then
+				RunConsoleCommand("ulx", "removeuserid", ply:SteamID())
+			elseif FAdmin then
+				RunConsoleCommand("fadmin", "setaccess", ply:SteamID(), "user")
+			elseif serverguard then
+				serverguard.player:SetRank(ply, "user")
+			else
+				ply:Ban(kick_reason)
+				return
+			end
+			ply:Kick(kick_reason)
 		end
 	end
-  
-  ply.SaCheckDelay = CurTime()+2 --Check delay
 end)
