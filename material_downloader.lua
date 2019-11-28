ea8745917eb67793379d3eae21f5483fcb2008a4
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2019-11-28 19:20:14 +0700

    Update material_downloader.lua

--- material_downloader.lua
+++ material_downloader.lua
@@ -24,12 +24,13 @@ end
 
 
 --———> Usage Exapmle:
+local img_size = {256, 241}
 hook.Add("HUDPaint", "IncredibleImgURLExample", function()
 	Mkdir("incredible_materials") -- its a custom func*
 	local mat = DownloadMaterial("https://incredible-gmod.ru/assets/other/be1zebub_pixelart.png", "incredible_materials/be1ze_pixel.png")
 	if not mat then return end
-	
-	surface.SetDrawColor(255, 255, 255, 255)
+		
+	surface.SetDrawColor(color_white)
 	surface.SetMaterial(mat)
-	surface.DrawTexturedRect(ScrW()/2-128, ScrH()/2-120.5, 256, 241)
+	surface.DrawTexturedRect(ScrW()/2 - img_size[1]/2, ScrH()/2 - img_size[2]/2, img_size[1], img_size[2])
 end)
