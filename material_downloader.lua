12367f2495dc3f54a93396c4fa27a5262fe10e49
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2019-11-28 19:14:24 +0700

    Update material_downloader.lua

--- material_downloader.lua
+++ material_downloader.lua
@@ -10,10 +10,26 @@ function DownloadMaterial(img_url, path)
     if file.Exists(path, "DATA") then
     	return Material("data/"..path, "noclamp smooth")
     else
+    	if (LocalPlayer().ImgURL_FetchDelay or 0) > CurTime() then return false end
+	LocalPlayer().ImgURL_FetchDelay = CurTime() + 5
         http.Fetch(img_url, function(result)
             if (result) then
                 file.Write(path, result)
             end
         end)
+        return false
     end
 end
+
+
+
+--———> Usage Exapmle:
+hook.Add("HUDPaint", "IncredibleImgURLExample", function()
+	Mkdir("incredible_materials") -- its custom func *
+	local mat = DownloadMaterial("https://incredible-gmod.ru/assets/other/be1zebub_pixelart.png", "incredible_materials/be1ze_pixel.png")
+	if not mat then return end
+	
+	surface.SetDrawColor(255, 255, 255, 255)
+	surface.SetMaterial(mat)
+	surface.DrawTexturedRect(ScrW()/2-128, ScrH()/2-120.5, 256, 241)
+end)
