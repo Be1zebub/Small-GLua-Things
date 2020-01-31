1ad1897e2864aa56faf871aaafd76416353d36f0
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-01-31 12:04:11 +0700

    Update sv_discord_post_lib.lua

--- sv_discord_post_lib.lua
+++ sv_discord_post_lib.lua
@@ -1,10 +1,10 @@
---[[—————————————————————————————————————
-        Incredible Discord Logging
-          Credits: [INC]Be1zebub
+--[[——————————————————————————————————————————————--
+			 Developer: [INC]Be1zebub
 
-	Visit my GModDayz Server:
-        http://incredible-gmod.ru
-—————————————————————————————————————]]--
+		 Website: incredible-gmod.ru/owner
+		EMail: beelzebub@incredible-gmod.ru
+		Discord: discord.incredible-gmod.ru
+--——————————————————————————————————————————————]]--
 
 local phpurl = "Your self hosted .php api interface, .php script from - https://github.com/Be1zebub/Small-GLua-Things/blob/master/inc_discord_post.php" --Link to your .php
 local RoflanChelik = "https://i.imgur.com/T3EE95z.png" --Default Avatar (if SteamAPI grants ERROR)
