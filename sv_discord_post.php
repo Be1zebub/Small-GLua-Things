3f1b0089d7b08d09cbf6223314e582ac87d68aef
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-01-31 12:05:27 +0700

    Update sv_discord_post.php

--- sv_discord_post.php
+++ sv_discord_post.php
@@ -1,4 +1,12 @@
 <?php 
+    /*—————————————————————————————————————————
+             Developer: [INC]Be1zebub
+
+         Website: incredible-gmod.ru/owner
+        EMail: beelzebub@incredible-gmod.ru
+        Discord: discord.incredible-gmod.ru
+    —————————————————————————————————————————*/
+    
     $avatar_url = $_POST["avatar_url"];
     $username = $_POST["username"];
     $content = $_POST["content"];
