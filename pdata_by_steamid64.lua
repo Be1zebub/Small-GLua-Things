1da13537763a9e9e3e059b1443797b998e663158
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-02-07 14:07:12 +0700

    Update pdata_by_steamid64.lua

--- pdata_by_steamid64.lua
+++ pdata_by_steamid64.lua
@@ -3,20 +3,19 @@
 
 local PMETA = FindMetaTable("Player")
 
-function PMETA:GetPData( name, default )
+function PMETA:GetPData(name, default)
 	name = Format( "%s[%s]", self:SteamID64(), name)
-	local val = sql.QueryValue("SELECT value FROM playerpdata WHERE infoid = " .. SQLStr(name) .. " LIMIT 1")
-	if val == nil then return default end
+	local val = sql.QueryValue("SELECT value FROM playerpdata WHERE infoid = "..SQLStr(name).." LIMIT 1")
 
-	return val
+	return val or default
 end
 
-function PMETA:SetPData( name, value )
+function PMETA:SetPData(name, value)
 	name = Format("%s[%s]", self:SteamID64(), name)
-	return sql.Query("REPLACE INTO playerpdata ( infoid, value ) VALUES ( " .. SQLStr(name) .. ", " .. SQLStr(value) .. " )") ~= false
+	return sql.Query("REPLACE INTO playerpdata (infoid, value) VALUES ("..SQLStr(name)..", "..SQLStr(value).." )") ~= false
 end
 
-function PMETA:RemovePData( name )
+function PMETA:RemovePData(name)
 	name = Format("%s[%s]", self:SteamID64(), name)
-	return sql.Query("DELETE FROM playerpdata WHERE infoid = " .. SQLStr(name)) ~= false
+	return sql.Query("DELETE FROM playerpdata WHERE infoid = "..SQLStr(name)) ~= false
 end
