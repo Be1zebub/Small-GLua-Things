aa7ab07da123054ae97b3dec3fd1a343b19a6789
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-03-05 15:46:00 +0700

    Update sh_getkeyname.lua

--- sh_getkeyname.lua
+++ sh_getkeyname.lua
@@ -3,8 +3,7 @@ local Mouse2Name = {
 	[MOUSE_4] = "MOUSE 4",
 	[MOUSE_5] = "MOUSE 5",
 	[MOUSE_WHEEL_UP] = "MOUSE WHEEL UP",
-	[MOUSE_WHEEL_DOWN] = "MOUSE WHEEL DOWN",
-	[NULL] = "UNKNOWN MOUSE"
+	[MOUSE_WHEEL_DOWN] = "MOUSE WHEEL DOWN"
 }
 
 local __a, __b, __c = isnumber, MOUSE_MIDDLE, input.GetKeyName
@@ -13,7 +12,7 @@ function GetKeyName(key)
 	if not __a(key) then return "UNKNOWN KEY" end
 
 	if key >= __b then
-		return Mouse2Name[key]
+		return Mouse2Name[key] or "UNKNOWN MOUSE"
 	end
 
 	return __c(key) and __c(key):upper() or "UNKNOWN KEY"
