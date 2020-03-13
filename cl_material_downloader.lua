3d577801ee347858f4f470f8be4c1e3d30330720
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-03-14 03:28:53 +0700

    Update cl_material_downloader.lua

--- cl_material_downloader.lua
+++ cl_material_downloader.lua
@@ -6,6 +6,7 @@
            Discord: discord.incredible-gmod.ru
 --———————————————————————————————————————————————————]]--
 local ImgURL_FetchDelay = 0
+
 function DownloadMaterial(img_url, path)
     if file.Exists(path, "DATA") then
     	return Material("data/"..path, "noclamp smooth")
@@ -13,7 +14,10 @@ function DownloadMaterial(img_url, path)
     	if ImgURL_FetchDelay > CurTime() then return false end
 	ImgURL_FetchDelay = CurTime() + 5
         http.Fetch(img_url, function(result)
-            if (result) then
+            if result then
+		if not file.Exists("incredible_materials", "DATA") then
+                    file.CreateDir("incredible_materials")
+                end
                 file.Write(path, result)
             end
         end)
@@ -26,7 +30,6 @@ end
 --———> Usage Exapmle:
 local img_size = {256, 241}
 hook.Add("HUDPaint", "IncredibleImgURLExample", function()
-	Mkdir("incredible_materials") -- its a custom func*
 	local mat = DownloadMaterial("https://incredible-gmod.ru/assets/other/be1zebub_pixelart.png", "incredible_materials/be1ze_pixel.png")
 	if not mat then return end
 		
