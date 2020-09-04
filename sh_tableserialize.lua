051af1ec80c9431e09671a5c07b0d16a66cfdfdf
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-09-05 05:41:03 +0700

    Update sh_tableserialize.lua

--- sh_tableserialize.lua
+++ sh_tableserialize.lua
@@ -1,17 +1,19 @@
+local strrepeat, pairs, type, format, tostring, print = string.rep, pairs, type, string.format, tostring, print
+
 function table.ToPlain(tbl, indent, out)
     local isend = not out
     out = out or "{\n"
 
     indent = indent or 1
     for k, v in pairs(tbl) do
-        local tabs = string.rep("   ", indent)
+        local tabs = strrepeat("   ", indent)
         formatting = tabs .. k .." = "
         if type(v) == "table" then
             out = out .. formatting .."{\n"
             out = out .. table.ToPlain(v, indent + 1, "")
             out = out .. tabs .."},\n"
         else
-            out = out .. formatting .. (type(v) == "string" and string.format("%q", v) or tostring(v)) ..",\n"    
+            out = out .. formatting .. (type(v) == "string" and format("%q", v) or tostring(v)) ..",\n"    
         end
     end
     if isend then out = out .."}\n" end
@@ -22,7 +24,7 @@ end
 function table.Print(tbl, indent)
     indent = indent or 0
     for k, v in pairs(tbl) do
-        formatting = string.rep("   ", indent) .. k .. ": "
+        formatting = strrepeat("   ", indent) .. k ..": "
         if type(v) == "table" then
             print(formatting)
             table.Print(v, indent + 1)
