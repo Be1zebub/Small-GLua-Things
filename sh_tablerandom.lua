de646a77d1919611a53a8a0d204222f079a033a1
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-09-06 16:28:59 +0700

    Update sh_tablerandom.lua

--- sh_tablerandom.lua
+++ sh_tablerandom.lua
@@ -1,8 +1,10 @@
 -- Thats better then a shitty glua table.Random function
 
+local random, GetKeys = math.random, table.GetKeys
+
 function table.Random(tab, issequential)
-    local keys = issequential and tab or table.GetKeys(tab)
-    local rand = keys[math.random(1, #keys)]
+    local keys = issequential and tab or GetKeys(tab)
+    local rand = keys[random(1, #keys)]
     return tab[rand], rand 
 end
 
