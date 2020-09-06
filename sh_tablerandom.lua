ff503c37c2deafb0038080300098332e3fa71b3f
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-09-06 16:27:44 +0700

    Create sh_tablerandom.lua

---
+++ sh_tablerandom.lua
@@ -0,0 +1,14 @@
+function table.Random(tab, issequential)
+    local keys = issequential and tab or table.GetKeys(tab)
+    local rand = keys[math.random(1, #keys)]
+    return tab[rand], rand 
+end
+
+--[[
+print(table.Random({
+    Hello = true,
+    World = 1,
+    Lorem = function() end,
+    Ispum = "dolor"
+}))
+]]--
