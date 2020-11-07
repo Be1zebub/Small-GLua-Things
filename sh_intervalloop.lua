cb7a17ca92f09ae1582fc23682e0b3b19c84c9ac
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-11-07 22:43:14 +0700

    Update sh_intervalloop.lua

--- sh_intervalloop.lua
+++ sh_intervalloop.lua
@@ -3,25 +3,49 @@
 -- Актуально в местах где не требуется мгновенный результат.
 
 local lnum = 0
-function IntervalLoop(time, tab, callback, notsequential)
+function IntervalLoop(time, tab, callback, notsequential, onfinish)
     lnum = lnum + 1
+    local uid = "IntervalLoop".. lnum
+    
     if notsequential then -- table.IsSequential это клёво, но хардкодинг не требует лишних циклов.
-        timer.Create("IntervalLoop"..lnum, time, table.Count(tab), function()
+        local count = table.Count(tab)
+        timer.Create(uid, time, count, function()
             local k, v = next(tab)
             callback(v, k)
             tab[k] = nil
         end)
+        
+        if onfinish then
+            timer.Simple(time * count, onfinish)
+        end
     else
+        local count = #tab
         local i = 0
-        timer.Create("IntervalLoop"..lnum, time, #tab, function()
+        timer.Create(uid, time, count, function()
             i = i + 1
             callback(tab[i], i)
         end)
+        
+        if onfinish then
+            timer.Simple(time * count, onfinish)
+        end
     end
     
     return {
         Stop = function()
-            timer.Remove("IntervalLoop"..lnum)
+            return timer.Stop(uid)
+        end,
+        Remove = function()
+            timer.Remove(uid)
+        end,
+        Pause = function()
+            return timer.Pause(uid)
+        end,
+        TimeLeft = function()
+            return timer.TimeLeft(uid)
+        end,
+        RepsLeft = function()
+            return timer.RepsLeft(uid) 
         end
     }
 end
