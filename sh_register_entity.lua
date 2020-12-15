8232b4c58b14ad78401cf66cfbb005ff2d0b2b54
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-12-15 23:17:29 +0700

    Create sh_register_entity.lua

---
+++ sh_register_entity.lua
@@ -0,0 +1,23 @@
+function util.RegisterEntity(class, callback)
+    ENT = {}
+
+    ENT.Base = "base_entity"
+    ENT.Type = "anim"
+    ENT.Author       = "Beelzebub"
+    ENT.Contact      = "beelzebub@incredible-gmod.ru"
+    ENT.Category     = "incredible-gmod.ru"
+
+    callback(ENT)
+
+    AddCSLuaFile("cl.lua")
+
+    if SERVER then
+        include("sv.lua")
+    else
+        include("cl.lua")
+    end
+
+    scripted_ents.Register(ENT, class)
+
+    ENT = nil
+end
