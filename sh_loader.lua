c66cbca4e91d581b861fafee023f8650c2479767
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-12-17 22:50:44 +0700

    Create sh_loader.lua

---
+++ sh_loader.lua
@@ -0,0 +1,61 @@
+-- incredible-gmod.ru
+
+local Loader = {}
+Loader.Debug = false
+
+Loader.include_realm = {
+    sv = SERVER and include or function() end,
+    cl = SERVER and AddCSLuaFile or include
+}
+
+include_realm.sh = function(f)
+    include_realm.sv(f)
+    include_realm.cl(f)
+end
+
+function Loader:GetFilename(path)
+	return path:match("[^/]+$")
+end
+
+function Loader:Include(fpath)
+	local realm = string.sub(self:GetFilename(fpath), 1, 2)
+
+	local func = self.include_realm[realm]
+	if func == nil then return false end
+
+	if self.Debug then
+		print(realm, fpath)
+	end
+
+	func(fpath)
+
+	return true
+end
+
+function Loader:IncludeDir(path, recurse, loaded)
+	loaded = loaded or {}
+
+	local files, folders = file.Find(path .."/*", "LUA")
+
+	for i, f in ipairs(files) do
+		local fpath = path .."/".. f
+		
+		if self:Include(fpath) then
+			table.insert(loaded, fpath)
+		end
+	end
+
+	if recurse then
+		for i, f in ipairs(folders) do
+			if self.Debug then
+				print("recurse", path .."/".. f)
+			end
+
+			include_dir(path .."/".. f, recurse, loaded)
+		end
+	end
+
+	return loaded
+end
+
+return Loader
