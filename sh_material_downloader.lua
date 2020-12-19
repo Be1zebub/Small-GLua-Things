fcc5f21f96b9f7ef13df44173cf29dc3b11dff74
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2020-12-20 02:44:29 +0700

    Create sh_material_downloader.lua

---
+++ sh_material_downloader.lua
@@ -0,0 +1,37 @@
+-- incredible-gmod.ru
+
+local file, Material, Fetch, find = file, Material, http.Fetch, string.find
+
+local errorMat = Material("error")
+local WebImageCache = {}
+function http.DownloadMaterial(url, path, callback)
+    if WebImageCache[url] then return callback(WebImageCache[url]) end
+
+    local data_path = "data/".. path
+    if file.Exists(path, "DATA") then
+        WebImageCache[url] = Material(data_path, "smooth", "noclamp")
+        callback(WebImageCache[url])
+    else
+        Fetch(url, function(img)
+            if img == nil or find(img, "<!DOCTYPE HTML>", 1, true) then return callback(errorMat) end
+            
+            file.Write(path, img)
+            WebImageCache[url] = Material(data_path, "smooth", "noclamp")
+            callback(WebImageCache[url])
+        end, function()
+            callback(errorMat)
+        end)
+    end
+end
+
+--[[ Example:
+http.DownloadMaterial("https://i.imgur.com/Msmeqkq.png", "beelze_pixel.png", function(example)
+    local w, h = 128, 128
+
+    hook.Add("HUDPaint", "http.DownloadMaterial", function()
+        surface.SetDrawColor(255, 255, 255)
+        surface.SetMaterial(example)
+        surface.DrawTexturedRect(ScrW() * 0.5 - w * 0.5, ScrH() * 0.5 - h * 0.5, w, h)
+    end)
+end)
+]]--
