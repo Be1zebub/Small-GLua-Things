70caad2a9c3b57c1d2c22bf8fd61fc65d40e841d
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2021-01-20 18:06:53 +0300

    Create hook.lua

---
+++ hook.lua
@@ -0,0 +1,25 @@
+hook._GetTable = hook._GetTable or hook.GetTable
+
+function hook.GetTable(eventName)
+    if eventName then
+        return hook._GetTable()[eventName] or {}
+    else
+        return hook._GetTable()
+    end
+end
+
+function hook.Get(eventName, identifier)
+    return hook.GetTable(eventName)[identifier]
+end
+
+function hook.Exists(eventName, identifier)
+    return tobool(hook.Get(eventName, identifier))
+end
+
+for ClassName, Class in pairs(debug.getregistry()) do
+    if istable(Class) and Class.IsValid and isstring(ClassName) then
+        function Class:AddHook(eventName, callback)
+            hook.Add(eventName, self, callback)
+        end
+    end
+end
