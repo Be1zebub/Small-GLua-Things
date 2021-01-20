a0edcd6caeb5f76cc71f6e7b485a79c0597e30d8
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2021-01-20 18:13:18 +0300

    Update sh_pdata.lua

--- sh_pdata.lua
+++ sh_pdata.lua
@@ -3,21 +3,21 @@
 -- This solution simply replaces the default pdata features, nothing more.
 
 local PMETA = FindMetaTable("Player")
-local Format, SQLStr, sql = Format, SQLStr, sql
+local Format, SQLStr, sql, SteamID64 = Format, SQLStr, sql, PMETA.SteamID64
 
 function PMETA:GetPData(name, default)
-	name = Format( "%s[%s]", self:SteamID64(), name)
+	name = Format( "%s[%s]", SteamID64(self), name)
 	local val = sql.QueryValue("SELECT value FROM playerpdata WHERE infoid = ".. SQLStr(name) .." LIMIT 1")
 
 	return val or default
 end
 
 function PMETA:SetPData(name, value)
-	name = Format("%s[%s]", self:SteamID64(), name)
+	name = Format("%s[%s]", SteamID64(self), name)
 	return sql.Query("REPLACE INTO playerpdata (infoid, value) VALUES (".. SQLStr(name) ..", ".. SQLStr(value) .." )") ~= false
 end
 
 function PMETA:RemovePData(name)
-	name = Format("%s[%s]", self:SteamID64(), name)
+	name = Format("%s[%s]", SteamID64(self), name)
 	return sql.Query("DELETE FROM playerpdata WHERE infoid = ".. SQLStr(name)) ~= false
 end
