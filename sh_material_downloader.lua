47a187f55a381d15677556e475152b009248d1f8
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2021-01-27 12:15:17 +0300

    Update sh_material_downloader.lua

--- sh_material_downloader.lua
+++ sh_material_downloader.lua
@@ -5,26 +5,30 @@ local file, Material, Fetch, find = file, Material, http.Fetch, string.find
 
 local errorMat = Material("error")
 local WebImageCache = {}
-function http.DownloadMaterial(url, path, callback)
+function http.DownloadMaterial(url, path, callback, retry_count)
     if WebImageCache[url] then return callback(WebImageCache[url]) end
 
     local data_path = "data/".. path
     if file.Exists(path, "DATA") then
-        WebImageCache[url] = Material(data_path, "smooth", "noclamp")
+        WebImageCache[url] = Material(data_path, "smooth noclamp")
         callback(WebImageCache[url])
     else
         Fetch(url, function(img)
             if img == nil or find(img, "<!DOCTYPE HTML>", 1, true) then return callback(errorMat) end
             
             file.Write(path, img)
-            WebImageCache[url] = Material(data_path, "smooth", "noclamp")
+            WebImageCache[url] = Material(data_path, "smooth noclamp")
             callback(WebImageCache[url])
         end, function()
-            callback(errorMat)
+            if retry_count and retry_count > 0 then
+                retry_count = retry_count - 1
+                http.DownloadMaterial(url, path, callback, retry_count)
+            else
+                callback(errorMat)
+            end
         end)
     end
 end
-
 --[[ Example:
 http.DownloadMaterial("https://i.imgur.com/Msmeqkq.png", "beelze_pixel.png", function(example)
     local w, h = 128, 128
