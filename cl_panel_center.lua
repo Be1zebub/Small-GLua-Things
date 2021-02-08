f0697f45341dab9fefa039bbed679a275d1275dc
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2021-02-08 14:15:43 +0300

    Update cl_panel_center.lua

--- cl_panel_center.lua
+++ cl_panel_center.lua
@@ -4,10 +4,10 @@ local Panel = FindMetaTable("Panel")
 
 function Panel:CenterX(fraction)
     fraction = fraction or 0.5
-    self.X = self:GetParent():GetWide() * fraction - self:GetWide() * fraction
+    self.X = self:GetParent():GetWide() * fraction - self:GetWide() * 0.5
 end
 
 function Panel:CenterY(fraction)
     fraction = fraction or 0.5
-    self.Y = self:GetParent():GetTall() * fraction - self:GetTall() * fraction
+    self.Y = self:GetParent():GetTall() * fraction - self:GetTall() * 0.5
 end
