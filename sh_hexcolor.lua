f86fb12f8afd645a364823b11693a9e22e9f358d
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2021-05-22 21:06:07 +0300

    Update sh_hexcolor.lua

--- sh_hexcolor.lua
+++ sh_hexcolor.lua
@@ -1,11 +1,13 @@
 local hexadecimal, rgb, hex, index
 local ipairs, tonumber = ipairs, tonumber
 local string_sub, string_len, math_fmod, math_floor = string.sub, string.len, math.fmod, math.floor
+local elems = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"}
+local len
 
 local Color_Cache = {}
 
-return function(...)
-	rgb = {...}
+return function(r, g, b)
+	rgb = {r, g, b}
 
 	if Color_Cache[rgb] then
 		return Color_Cache[rgb]
@@ -19,12 +21,13 @@ return function(...)
 		while value > 0 do
 			index = math_fmod(value, 16) + 1
 			value = math_floor(value / 16)
-			hex = string_sub("0123456789ABCDEF", index, index) .. hex			
+			hex = elems[index] .. hex			
 		end
 
-		if string_len(hex) == 0 then
+		len = string_len(hex)
+		if len == 0 then
 			hex = "00"
-		elseif string_len(hex) == 1 then
+		elseif len == 1 then
 			hex = "0".. hex
 		end
 
