453a64b24bd8ca1d4fcffed873aa4464b3b147de
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2021-07-25 18:35:07 +0300

    Update sh_material.lua

--- sh_material.lua
+++ sh_material.lua
@@ -3,14 +3,15 @@
 
 if not istable(Material) then
     local OMaterial = Material
-    Material = setmetatable({}, {
+    Material = setmetatable({
+        cache = {},
+        old = OMaterial
+    }, {
         __call = function(self, path, ...)
             if self.cache[path] then return self.cache[path] end
             local m = self.old(path, "smooth mips")
             self.cache[path] = m
             return m
-        end,
-        cache = {},
-        old = OMaterial
+        end
     })
 end
