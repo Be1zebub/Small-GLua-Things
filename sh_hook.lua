aa19cbc62d6f984086e0b4ee14b6a077f7a81f66
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2021-09-30 09:22:56 +0300

    Update sh_hook.lua

--- sh_hook.lua
+++ sh_hook.lua
@@ -19,19 +19,19 @@ function hook.Exists(eventName, identifier)
     return tobool(hook.Get(eventName, identifier))
 end
 
+function hook.Once(eventName, callback, identifier)
+    identifier = identifier or debug.traceback()
+
+    hook.Add(eventName, identifier, function()
+        hook.Remove(eventName, identifier)
+        callback()
+    end)
+end
+
 for ClassName, Class in pairs(debug.getregistry()) do
     if istable(Class) and Class.IsValid and isstring(ClassName) then
         function Class:AddHook(eventName, callback)
             hook.Add(eventName, self, callback)
         end
     end
-end
-
-function hook.Once(eventName, callback, identifier)
-    identifier = identifier or SysTime()
-
-    hook.Add(eventName, identifier, function()
-        hook.Remove(eventName, identifier)
-        callback()
-    end)
 end
