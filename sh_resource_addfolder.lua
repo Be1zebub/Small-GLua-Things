46e3a5617c3e8e766605330149589890bf7119e2
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2022-01-21 22:29:20 +0300

    Update sh_resource_addfolder.lua

--- sh_resource_addfolder.lua
+++ sh_resource_addfolder.lua
@@ -3,18 +3,18 @@
 function resource.AddFolder(name, recurse)
     local files, folders = file.Find(name .."/*", "GAME")
 
-    for k, fname in pairs(files) do
+    for i, fname in ipairs(files) do
         resource.AddSingleFile(name .."/".. fname)
     end
 
     if recurse then
-        for k, fname in pairs(folders) do
+        for i, fname in ipairs(folders) do
             resource.AddFolder(name .."/".. fname, recurse)
         end
     end
 end
 
---[[ Example:
-resource.AddFolder("materials/chatbox", true) -- chatbox icons
-resource.AddFolder("resource", true) -- fonts
+--[[ Пример:
+resource.AddFolder("materials/chatbox", true) -- иконочки чатика
+resource.AddFolder("resource/fonts/notosans_*.ttf", false) -- шрифтики
 ]]--
