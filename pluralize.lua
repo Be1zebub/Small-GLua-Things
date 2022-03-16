1958cf6618326b4a26e2244196263f1b4c0ba1b4
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2022-03-16 03:01:54 +0300

    Update pluralize.lua

--- pluralize.lua
+++ pluralize.lua
@@ -1,12 +1,13 @@
 -- incredible-gmod.ru
 -- https://github.com/Be1zebub/Small-GLua-Things/blob/master/pluralize.lua
+-- souse: https://internetbrains.blogspot.com/2010/01/javascript.html
 
 local cases = {[0] = 3, [1] = 1, [2] = 2, [3] = 2, [4] = 2, [5] = 3}
 function pluralize(n, titles)
     n = math.abs(n)
     return titles[
-        (n % 100 > 4 and n % 100 < 20) and 2 or
-        cases[(n % 10 < 5) and n % 10 or 5]
+        (n % 100 > 4 and n % 100 < 20) and 3 or
+        cases[(n % 10 < 5) and n % 10 or 6]
     ]
 end
 
@@ -21,9 +22,9 @@ end
 2    секунды
 3    секунды
 4    секунды
-5    секунды
-6    секунды
-7    секунды
-8    секунды
-9    секунды
+5    секунд
+6    секунд
+7    секунд
+8    секунд
+9    секунд
 ]]--
