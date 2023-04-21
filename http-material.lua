9e7deece3de9984379eef5bcc16812740ef424f8
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2023-04-21 15:41:30 +0300

    http-material.lua minor improvements

--- http-material.lua
+++ http-material.lua
@@ -13,18 +13,18 @@ end)
 file.CreateDir("http-material")
 
 local httpMaterial = {}
-httpMaterial.__index = httpMaterial
 
 function httpMaterial:Init(url, flags, ttl, cback)
 	ttl = ttl or 86400
 
-	local fname = url:match("([^/]+)$"):gsub("[&?]([^/%s]+)=([^/%s]+)", "")
-	if fname:match("^.+(%..+)$") == nil then
+	local fname = url:match("([^/]+)$") -- get content after latest slash
+	:gsub("[&?]([^/%s]+)=([^/%s]+)", "") -- strip query
+
+	if fname:match("^.+(%..+)$") == nil then -- if filename have no extension
 		fname = fname ..".png"
 	end
 
-	local uid = util.CRC(url)  .."_".. fname
-	local path = "http-material/".. uid
+	local path = "http-material/".. util.CRC(url) .."_".. fname
 
 	if file.Exists(path, "DATA") and file.Time(path, "DATA") + ttl > os.time() then
 		self.material = Material("data/".. path, flags)
@@ -36,7 +36,7 @@ function httpMaterial:Init(url, flags, ttl, cback)
 				self.material = Material("data/".. path, flags)
 				if cback then cback(self.material) end
 			else
-				ErrorNoHalt(string.format("Cant download http-material! Url: %s, reason: %s\n", url, reason))
+				ErrorNoHalt(string.format("Cant download http-material! Url: %s, reason: %s\nTryin with proxy...\n", url, reason))
 
 				url = "https://proxy.duckduckgo.com/iu/?u=".. url
 				self:Download(url, function(succ, result)
@@ -69,6 +69,7 @@ function httpMaterial:Download(url, cback, retry)
 	http.Fetch(url, function(raw, _, _, code)
 		if not raw or raw == "" or code ~= 200 or raw:find("<!DOCTYPE HTML>", 1, true) then
 			if retry - 1 <= 0 then return cback(false, "retry") end
+
 			self:Download(url, cback, retry - 1)
 			return
 		end
@@ -76,6 +77,7 @@ function httpMaterial:Download(url, cback, retry)
 		cback(true, raw)
 	end, function(err)
 		if retry - 1 <= 0 then return cback(false, err) end
+
 		self:Download(url, cback, retry - 1)
 	end)
 end
@@ -91,22 +93,13 @@ function httpMaterial:Draw(x, y, w, h, angle)
 	surface[angle and "DrawTexturedRectRotated" or "DrawTexturedRect"](x, y, w, h, angle)
 end
 
-setmetatable(httpMaterial, {
-	__call = httpMaterial.Draw
-})
+return function(url, flags, ttl, cback)
+	local instance = setmetatable({}, {
+		__index = httpMaterial,
+		__call = httpMaterial.Draw
+	})
 
-local function new(url, flags, ttl, cback)
-	local instance = setmetatable({}, httpMaterial)
 	instance:Init(url, flags, ttl, cback)
+
 	return instance
 end
-
---[[ test:
-local gorgeous = new("https://github.com/Be1zebub/elite-emotes-collection/blob/main/gorgeous/pleasure.png?raw=true", "smooth mips")
-hook.Add("HUDPaint", "incredible-gmod.ru/http-material", function()
-	surface.SetDrawColor(255, 255, 255)
-	gorgeous:Draw(32, 32, 64, 64)
-end)
-]]--
-
-return new
