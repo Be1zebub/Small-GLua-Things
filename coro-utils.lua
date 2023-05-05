41ed4cdf9ec9ac74c0c14b9c25bb1d87478d860f
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2023-05-05 22:04:07 +0300

    Create coro-utils.lua

---
+++ coro-utils.lua
@@ -0,0 +1,72 @@
+-- from incredible-gmod.ru with <3
+-- https://github.com/Be1zebub/Small-GLua-Things/blob/master/coro-utils.lua
+
+--[[ Example:
+coroutine.wrap(function()
+	print(SysTime())
+	coroutine.pause(1.5)
+	print(SysTime())
+
+	local succ, response, headers, code = coroutine.httpFetch("https://incredible-gmod.ru/ping.php")
+	if succ then
+		print("coroutine.httpFetch response: ".. response)
+	else
+		print("coroutine.httpFetch fail, reason: ".. response)
+	end
+end)()
+]]--
+
+function coroutine.pause(delay)
+	local co = coroutine.running()
+
+	timer.Simple(delay, function()
+		coroutine.resume(co)
+	end)
+
+	coroutine.yield()
+end
+
+function coroutine.async(fn, vararg)
+	return function()
+		local co = coroutine.running()
+
+		if vararg then
+			fn(function(succ, err, ...)
+				if succ == false then
+					ErrorNoHaltWithStack(err .."\n")
+				end
+
+				coroutine.resume(co, err, ...)
+			end, unpack({...}))
+		else
+			fn(function(succ, err, arg)
+				if succ == false then
+					ErrorNoHaltWithStack(err .."\n")
+				end
+
+				coroutine.resume(co, err, arg)
+			end, arg)
+		end
+
+		return coroutine.yield()
+	end
+end
+
+coroutine.http = coroutine.async(function(cback, HTTPRequest)
+	HTTPRequest.success = function(code, body, headers)
+		cback(true, body, headers, code)
+	end
+	HTTPRequest.failed = function(err)
+		cback(false, err)
+	end
+
+	HTTP(HTTPRequest)
+end, false)
+
+function coroutine.httpFetch(url, headers)
+	return coroutine.http({method = "GET", url = url, headers = headers})
+end
+
+function coroutine.httpPost(url, headers, body)
+	return coroutine.http({method = "POST", url = url, headers = headers, body = body})
+end
