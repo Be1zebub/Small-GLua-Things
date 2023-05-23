b47bee98c434645556b8844df959f63667894509
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2023-05-23 04:19:29 +0300

    Update multicolor-text.lua
    now it can split individual strings, its try to split by word - if it cant fit, it split wide too word to chars

--- multicolor-text.lua
+++ multicolor-text.lua
@@ -2,7 +2,9 @@
 -- https://github.com/Be1zebub/Small-GLua-Things/blob/master/multicolor-text.lua
 
 -- it can render multi-line + multi-color text
--- preview: https://cdn.discordapp.com/attachments/826589821908287498/1110215901569093723/2023-05-22_21-33-49.mp4
+-- this is a heavy function, you shouldnt call it every frame - use render targets to render text 1 time and copy it to the material
+-- learn how to use rendertargets here: https://wiki.facepunch.com/gmod/render.PushRenderTarget
+-- preview: https://cdn.discordapp.com/attachments/565108080300261398/1110373812970725377/2023-05-23_08-08-42.mp4
 
 function surface.DrawMulticolorText(x, y, font, text, maxW)
 	surface.SetTextColor(255, 255, 255)
@@ -21,15 +23,44 @@ function surface.DrawMulticolorText(x, y, font, text, maxW)
 		if isstring(v) then
 			w, h = surface.GetTextSize(v)
 
-			if maxW and x + w >= maxW then
-				x, y = baseX, y + lineHeight
-				v = v:gsub("^%s+", "")
+			if maxW and x + w > maxW then
+				v:gsub("(%s?[%S]+)", function(word)
+					w, h = surface.GetTextSize(word)
+
+					if x + w >= maxW then
+						x, y = baseX, y + lineHeight
+						word = word:gsub("^%s+", "")
+						w, h = surface.GetTextSize(word)
+
+						if x + w >= maxW then
+							word:gsub("[%z\x01-\x7F\xC2-\xF4][\x80-\xBF]*", function(char)
+								w, h = surface.GetTextSize(char)
+
+								if x + w >= maxW then
+									x, y = baseX, y + lineHeight
+								end
+
+								surface.SetTextPos(x, y)
+								surface.DrawText(char)
+
+								x = x + w
+							end)
+
+							return
+						end
+					end
+
+					surface.SetTextPos(x, y)
+					surface.DrawText(word)
+
+					x = x + w
+				end)
+			else
+				surface.SetTextPos(x, y)
+				surface.DrawText(v)
+
+				x = x + w
 			end
-
-			surface.SetTextPos(x, y)
-			surface.DrawText(v)
-
-			x = x + w
 		else
 			surface.SetTextColor(v.r, v.g, v.b, v.a)
 		end
@@ -38,38 +69,19 @@ function surface.DrawMulticolorText(x, y, font, text, maxW)
 	return x, y
 end
 
-render.PushRenderTarget(GetRenderTarget("r*b text 123", 200, ScrH() - 32))
-render.Clear(0, 0, 0, 0, true, true)
-
-cam.Start2D()
-	surface.DrawMulticolorText(32, 32, "DermaLarge", {
-		"Hello",
-		Color(255, 0, 0), " world!",
-		Color(0, 255, 0), " lorem ipsum",
-		Color(0, 0, 255), " dolor sit amet!"
-	}, 200)
-cam.End2D()
-
-render.PopRenderTarget()
-
-local text = CreateMaterial("r*b text 123", "UnlitGeneric", {
-	["$translucent"] = 1,
-	["$basetexture"] = "r*b text 123"
-})
+local x, y = 0, 0
 
 hook.Add("HUDPaint", "r*b text", function()
-	local maxW = 64 + (SysTime() * 250) % 500
-	local x, y = surface.DrawMulticolorText(256, 32, "DermaLarge", {
+	local maxW = gui.MouseX() - 256
+
+	surface.SetDrawColor(0, 0, 0, 125)
+	surface.DrawRect(256, 32, maxW, y)
+
+	x, y = surface.DrawMulticolorText(256, 32, "DermaLarge", {
 		"Hello",
 		Color(255, 0, 0), " world!",
 		Color(0, 255, 0), " lorem ipsum",
-		Color(0, 0, 255), " dolor sit amet!"
+		Color(0, 0, 255), " dolor sit amet!",
+		Color(255, 255, 255), " consecteturadipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 	}, maxW)
-
-	surface.SetDrawColor(255, 255, 255)
-	surface.DrawRect(256, 32 + y, maxW, 2)
-
-	surface.SetDrawColor(255, 255, 255)
-	surface.SetMaterial(text)
-	surface.DrawTexturedRect(0, 0, 200, ScrH() - 32)
 end)
