64f829658a6fb55bb5096b3241ab492d81fddde2
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2023-07-15 22:00:32 +0700

    Update loader_min.lua
    +debugging https://cdn.discordapp.com/attachments/677151280225058826/1129789454299693147/image.png

--- loader_min.lua
+++ loader_min.lua
@@ -1,6 +1,14 @@
 -- from incredible-gmod.ru with <3
 -- https://github.com/Be1zebub/Small-GLua-Things/blob/master/loader_min.lua
 
+local debug = true
+
+local include_realm_order = {
+	["sh"] = 1,
+	["sv"] = 2,
+	["cl"] = 3
+}
+
 local include_realm = {
 	sv = SERVER and include or function() end,
 	cl = SERVER and AddCSLuaFile or include,
@@ -10,15 +18,60 @@ local include_realm = {
 	end
 }
 
-local include_realm_order = {
-	["sh"] = 1,
-	["sv"] = 2,
-	["cl"] = 3
-}
+local dirColor
 
-local function IncludeDir(path, storage)
+if debug then
+	if SERVER then
+		local function FormatColor(r, g, b)
+			return string.format("\27[38;2;%d;%d;%dm", r, g, b)
+		end
+
+		local reset = "\27[0m "
+
+		local debug_realms = {
+			sv = FormatColor(3, 169, 244) .."server".. reset,
+			cl = FormatColor(222, 169, 9) .."client".. reset,
+			sh = FormatColor(3, 169, 244) .."sha".. FormatColor(222, 169, 9) .."red".. reset,
+		}
+
+		function debug(realm, path, lvl)
+			MsgN(string.rep("\t", lvl - 1) .. debug_realms[realm] .. path)
+		end
+
+		dirColor = FormatColor(100, 150, 255)
+	else
+		local debug_realms = {
+			sv = function()
+				return Color(3, 169, 244), "server "
+			end,
+			cl = function()
+				return Color(222, 169, 9), "client "
+			end,
+			sh = function()
+				return Color(222, 169, 9), "sha", Color(3, 169, 244), "red "
+			end
+		}
+
+		local white = Color(255, 255, 255)
+
+		function debug(realm, path, lvl)
+			MsgC(string.rep("\t", lvl - 1), debug_realms[realm]())
+			MsgC(white, path, "\n")
+		end
+	end
+else
+	debug = function() end
+end
+
+local function IncludeDir(path, storage, _lvl)
 	if path[#path] ~= "/" then
 		path = path .."/"
+	end
+	_lvl = _lvl or 1
+
+	if debug then
+		MsgN(dirColor, string.rep("\t", _lvl - 1), "> IncludeDir(\"".. path:sub(1, #path - 1) .."\")")
+		_lvl = _lvl + 1
 	end
 
 	local files, folders = file.Find(path .."*", "LUA")
@@ -33,17 +86,29 @@ local function IncludeDir(path, storage)
 			return include_realm_order[realm_a] < include_realm_order[realm_b] -- by realm sh > sv > cl
 		end
 
-		return a < b -- alphabetically
+		return a:sub(3) < b:sub(3) -- alphabetically
 	end)
 
 	for _, f in ipairs(files) do
-		local load = include_realm[f:sub(1, 2)] or include_realm.sh
+		local realm = f:sub(1, 2)
+		realm = include_realm[realm] and realm or "sh"
+		local fpath = path .. f
 
-		if storage then storage[path .. f] = load(path .. f) end
+		include_realm[realm](fpath)
+		if storage then storage[fpath] = v end
+		debug(realm, fpath, _lvl)
 	end
 
 	for _, f in ipairs(folders) do
-		IncludeDir(path .. f, storage)
+		local fpath = path .. f
+
+		local dir
+		if storage then
+			dir = {isdir = true}
+			storage[fpath] = dir
+		end
+
+		IncludeDir(fpath, dir, _lvl + 1)
 	end
 
 	return storage
