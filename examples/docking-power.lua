dfed7255bf03beb3617e39abe3810266a71887fa
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2024-06-13 23:53:59 +0700

    Create docking-power.lua

---
+++ examples/docking-power.lua
@@ -0,0 +1,36 @@
+-- Panel:Dock usage example
+
+if IsValid(_DockingPower) then
+	_testDock:Remove()
+end
+
+_DockingPower = vgui.Create("DPanel")
+_DockingPower:SetSize(512, 512)
+_DockingPower:Center()
+
+local function PaintColor(me, w, h)
+	surface.SetDrawColor(me.backgroundColor)
+	surface.DrawRect(0, 0, w, h)
+end
+
+local left = _DockingPower:Add("EditablePanel")
+left:SetWide(256 - 16)
+left:Dock(LEFT)
+left:DockMargin(0, 0, 32, 0)
+left.backgroundColor = Color(255, 73, 94)
+left.Paint = PaintColor
+left.Think = function(me)
+	me:SetWide(256 + 64 * math.sin(SysTime() * 2))
+end
+
+local top = _DockingPower:Add("EditablePanel")
+top:SetTall(256 - 16)
+top:Dock(TOP)
+top.backgroundColor = Color(52, 255, 94)
+top.Paint = PaintColor
+
+local fill = _DockingPower:Add("EditablePanel")
+fill:Dock(FILL)
+fill:DockMargin(0, 32, 0, 0)
+fill.backgroundColor = Color(52, 73, 255)
+fill.Paint = PaintColor
