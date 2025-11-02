8b03d92b8dfb29761f85811bd9a9d3a54ce2f5fd
Beelzebub <contact@gmod.one>
2025-11-02 16:26:42 +0300

    Rework localStorage
    nextgen xd

--- libs/localStorage.lua
+++ libs/localStorage.lua
@@ -1,31 +1,105 @@
 -- from gmod.one with <3
 -- https://github.com/Be1zebub/Small-GLua-Things/blob/master/libs/localStorage.lua
 
--- js localStorage port with extended features.
--- see unit tests at EOF for examples.
+-- js localStorage inspired, extended features included.
+-- see unit tests for examples.
 
-sql.Query("CREATE TABLE IF NOT EXISTS `localStorage` (`key` TEXT PRIMARY KEY, `value` TEXT)")
-
-local db = {cache = {}, initializedCache = {}}
 local unitTestsReserve = {
-	keys = {
-		___unitTestString = true,
-		___unitTestNumber = true,
-		___unitTestTable = true
-	},
-	enable = true
+	__unitTestString = true,
+	__unitTestNumber = true,
+	__unitTestTable = true
 }
 
-function db:Get(key, decoder)
-	key = tostring(key)
+local DB = {} -- database driver
 
-	if unitTestsReserve.enable and unitTestsReserve.keys[key] then
-		ErrorNoHalt("localStorage key \"" .. key .. "\" is reserved for unit tests, sorry you can't use it.\n")
+function DB:New(parent, name)
+	name = "local-storage/" .. name
+
+	sql.Query("CREATE TABLE IF NOT EXISTS `" .. name .. "` (`key` TEXT PRIMARY KEY, `value` TEXT)")
+
+	return setmetatable({
+		parent = parent,
+		name = name,
+		cache = {},
+		initializedCache = {},
+		queries = {
+			get = "SELECT `value` FROM `" .. name .. "` WHERE `key` = %s;",
+			set = "REPLACE INTO `" .. name .. "` (`key`, `value`) VALUES (%s, %s);",
+			delete = "DELETE FROM `" .. name .. "` WHERE `key` = %s;",
+			clear = "DELETE FROM `" .. name .. "`; VACUUM;",
+		},
+		runningUnitTests = false
+	}, {__index = DB})
+end
+
+
+function DB:RunUnitTests()
+	self.runningUnitTests = true -- allow using reserved keys for unit tests, regular usage of this keys is blocked.
+
+	local localStorage = self.parent -- its basically the localStorage instance, you make it like this myAddon.storage = lib.localStorage("myAddon")
+
+	do  -- test random datatype key will work (syntax sugar will make tostring for keys)
+		local testKey = function() end
+
+		localStorage[testKey] = "0"
+		assert(localStorage[testKey] == "0", "testKey should be 0")
+
+		localStorage[testKey] = nil
+		assert(localStorage[testKey] == nil, "testKey should be nil")
+	end
+
+	do -- basic test string key (syntax sugar will make tostring for keys)
+		localStorage.__unitTestString = nil
+		assert(localStorage.__unitTestString == nil, "testString value should be nil")
+
+		localStorage.__unitTestString = 1
+		assert(localStorage.__unitTestString == "1", "testString should be string 1")
+
+		localStorage.__unitTestString = nil
+	end
+
+	do -- test encoding/decoding (if string datatype is not enough, use :Set/:Get with encoder/decoder instead of syntax sugar)
+		localStorage:Set("__unitTestNumber", 1, tonumber)
+		assert(isnumber(localStorage:Get("__unitTestNumber", tonumber)), "testNumber should be number")
+
+		localStorage.__unitTestNumber = nil
+		assert(localStorage.__unitTestNumber == nil, "testNumber should be nil")
+	end
+
+	do -- test advanced encoding/decoding (same, but lets try a complex encoder/decoder to make sure everything works good)
+		localStorage:Set("__unitTestTable", {john = "doe"}, util.TableToJSON)
+		assert(localStorage:Get("__unitTestTable", util.JSONToTable).john == "doe", "testTable.john should be doe")
+
+		localStorage.__unitTestTable = nil
+	end
+
+	self.runningUnitTests = false
+
+	(function() -- test if reserved keys are not allowed to use (this keys are allowed to use only when unit tests are running, runningUnitTests thing)
+		local succ = pcall(localStorage.Set, localStorage, "__unitTestTable", "test")
+		assert(succ == false, "localStorage:Set should fail for reserved key! Unit tests are broken")
+	end)()
+end
+
+function DB:ValidateKey(key)
+	local originalKey = tostring(key)
+
+	key = originalKey:gsub("`", "``"):gsub("[%c]", "") -- escape backticks & remove control characters
+	assert(#key > 0, "localStorage key `" .. originalKey .. "` is empty or contains only control characters!")
+
+	if self.runningUnitTests ~= true and unitTestsReserve[key] then
+		error("localStorage key `" .. key .. "` is reserved for unit tests, sorry you can't use it.")
 		return
 	end
 
+	return key
+end
+
+function DB:Get(key, decoder)
+	key = self:ValidateKey(key)
+
 	if self.initializedCache[key] == nil then
-		local value = sql.QueryValue("SELECT `value` FROM `localStorage` WHERE `key` = " .. sql.SQLStr(key) .. ";")
+		local value = sql.QueryValue(self.queries.get:format(sql.SQLStr(key)))
 
 		if decoder and value then
 			value = decoder(value)
@@ -38,95 +112,94 @@ function db:Get(key, decoder)
 	return self.cache[key]
 end
 
-function db:Set(key, value, encoder)
-	key = tostring(key)
 
-	if unitTestsReserve.enable and unitTestsReserve.keys[key] then
-		ErrorNoHalt("localStorage key \"" .. key .. "\" is reserved for unit tests, sorry you can't use it.\n")
-		return
-	end
+function DB:Set(key, value, encoder)
+	key = self:ValidateKey(key)
 
 	if value == nil then
-		sql.Query("DELETE FROM `localStorage` WHERE `key` = " .. sql.SQLStr(key) .. ";")
+		sql.Query(self.queries.delete:format(sql.SQLStr(key)))
 	else
 		encoder = encoder or tostring
 		value = encoder(value)
 
-		sql.Query("REPLACE INTO `localStorage` (`key`, `value`) VALUES (" .. sql.SQLStr(key) .. ", " .. sql.SQLStr(value) .. ");")
+		sql.Query(self.queries.set:format(sql.SQLStr(key), sql.SQLStr(value)))
 	end
 
 	self.cache[key] = value
 end
 
-function db:Clear()
+function DB:Clear()
 	self.cache = {}
 	self.initializedCache = {}
 
-	sql.Query("DELETE FROM `localStorage`; VACUUM;")
+	sql.Query(self.queries.clear)
 end
 
-_G.localStorage = setmetatable({
-	setItem = function(key, value, encoder)
-		db:Set(key, value, encoder)
+----------------------
+
+local STORAGE = {} -- local storage driver
+
+function STORAGE:New(name)
+	local instance = {
+		name = name,
+		db = nil,
+	}
+
+	local db = DB:New(instance, name)
+	instance.db = db
+
+	setmetatable(instance, {
+		__newindex = function(me, key, value)
+			me:Set(key, value, nil)
+		end,
+		__index = function(me, key)
+			return STORAGE[key] or me:Get(key, nil)
+		end
+	})
+
+	db:RunUnitTests()
+
+	return instance
+end
+
+function STORAGE:Set(key, value, encoder)
+	self.db:Set(key, value, encoder)
+end
+
+function STORAGE:Get(key, decoder)
+	return self.db:Get(key, decoder)
+end
+
+function STORAGE:Clear()
+	self.db:Clear()
+end
+
+local globalStorage = nil
+local lib = {
+	localStorage = function(name) -- use this in most cases
+		assert(isstring(name), "localStorage name must be a string")
+		assert(name ~= "__globalStorage", "localStorage name cant be a __globalStorage, it's reserved")
+
+		local originalKey = name
+		name = name:gsub("`", "``"):gsub("[%c]", "") -- escape backticks & remove control characters
+		assert(#name > 0, "localStorage name `" .. originalKey .. "` is empty or contains only control characters!")
+
+		return STORAGE:New(name)
 	end,
-	getItem = function(key, decoder)
-		return db:Get(key, decoder)
+	globalStorage = function() -- meh, idk why someone would need this, but at least you got a free cache -_-
+		if globalStorage == nil then
+			globalStorage = STORAGE:New("__globalStorage")
+		end
+
+		return globalStorage
+	end
+}
+
+return setmetatable(lib, {  -- prevent modifying lib table
+	__index = function(_, key)
+		return lib[key]
 	end,
-	removeItem = function(key)
-		db:Set(key, nil)
-	end,
-	clear = function()
-		db:Clear()
-	end,
-}, {
-	__newindex = function(_, key, value) -- syntax sugar, its like setItem, but doesn't support encoder (always encodes value to string datatype)
-		db:Set(key, value)
-	end,
-	__index = function(_, key) -- syntax sugar, its like getItem, but doesn't support decoder (always returns string datatype)
-		return db:Get(key)
+	__newindex = function()
+		error("Nope, you can't modify localStorageLib table, it's read only.")
 	end
 })
-
--- unit tests
-unitTestsReserve.enable = false
-
-do  -- test random datatype key will work
-	local testKey = function() end
-
-	localStorage[testKey] = "0"
-	assert(localStorage[testKey] == "0", "testKey should be 0")
-
-	localStorage[testKey] = nil
-	assert(localStorage[testKey] == nil, "testKey should be nil")
-end
-
-do -- basic test string key
-	localStorage.___unitTestString = nil
-	assert(localStorage.___unitTestString == nil, "testString value should be nil")
-
-	localStorage.___unitTestString = 1
-	assert(localStorage.___unitTestString == "1", "testString should be string 1")
-
-	localStorage.___unitTestString = nil
-end
-
-do -- test encoding/decoding
-	localStorage.setItem("___unitTestNumber", 1, tonumber)
-	assert(isnumber(localStorage.getItem("___unitTestNumber", tonumber)), "testNumber should be number")
-
-	localStorage.___unitTestNumber = nil
-	assert(localStorage.___unitTestNumber == nil, "testNumber should be nil")
-end
-
-do -- test advanced encoding/decoding
-	localStorage.setItem("___unitTestTable", {john = "doe"}, util.TableToJSON)
-	assert(localStorage.getItem("___unitTestTable", util.JSONToTable).john == "doe", "testTable.john should be doe")
-
-	localStorage.___unitTestTable = nil
-end
-
-unitTestsReserve.enable = true
-(function() -- test reserved keys are not allowed to use
-	local succ = pcall(localStorage.setItem, "___unitTestTable", "test")
-	assert(succ == false, "setItem should fail for reserved key")
-end)()
